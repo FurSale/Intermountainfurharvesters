01e57e266a0e94806c376a42018dcc184627c6ba
Delbert Johnson <johnsondelbert1@gmail.com>
2019-11-17 01:08:04 -0800

    1

--- includes/end_html.php
+++ includes/end_html.php
@@ -23,6 +23,7 @@
     <!-- ================================================
     Scripts
     ================================================ -->
+    <script type="text/javascript" src="pwabuilder-sw-register.js"></script>
     <!-- jQuery Library -->
     <script type="text/javascript" src="../../vendors/jquery-3.2.1.min.js"></script>
     <!--materialize js-->
--- template.php
+++ template.php
@@ -5,5 +5,4 @@
 	);
 	require_once("includes/begin_html.php");
 ?>
-<script type="text/javascript" src="pwabuilder-sw-register.js"></script>
 <?php include 'includes/end_html.php'; ?>
