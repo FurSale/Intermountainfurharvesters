3608ad73d2419af189931960a31906b895ac36ef
Delbert Johnson <johnsondelbert1@gmail.com>
2019-11-16 17:48:36 -0800

    0.0.4

---
+++ pages/sellers/edit_sellers.php
@@ -0,0 +1,9 @@
+<?php
+	$pgsettings = array(
+		"title" => "Edit / Sellers",
+		"icon" => "icon-newspaper"
+	);
+	require_once("../../includes/begin_html.php");
+?>
+
+<?php include '../../includes/end_html.php'; ?>
