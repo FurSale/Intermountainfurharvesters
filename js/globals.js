1180972c8a89be99dde6f1200556bfa467b08989
Delbert Johnson <johnsondelbert1@gmail.com>
2019-11-17 02:08:15 -0800

    Update globals.js

--- js/globals.js
+++ js/globals.js
@@ -1,22 +1,3 @@
-(function() {
-
-  var allNavItems = $(".main-nav li");
-  var allTabs = $(".tab");
-
-  allNavItems.on("click", function(e) {
-
-    e.preventDefault();
-
-    allNavItems.removeClass("active");
-    allTabs.removeClass("active");
-
-    // this = list item
-    $(this).addClass("active");
-
-    var selector = $(this).find("a").attr("href");
-    $(selector).addClass("active");
-
-  });
 
   // ServiceWorker is a  progressive technology. Ignore unsupported browsers
   if ('serviceWorker' in navigator) {
