dadd0a5c677ea1cc78d870e01941b4cb3b205d86
Delbert Johnson <johnsondelbert1@gmail.com>
2019-11-20 23:34:08 -0800

    Update plugins.js

--- js/plugins.js
+++ js/plugins.js
@@ -12,6 +12,66 @@
     "use strict";
 
     var window_width = $(window).width();
+
+
+    // Search class for focus
+    $('.header-search-input').focus(
+      function() {
+        $(this).parent('div').addClass('header-search-wrapper-focus');
+      }).blur(
+      function() {
+        $(this).parent('div').removeClass('header-search-wrapper-focus');
+      });
+
+    // Check first if any of the task is checked
+    $('#task-card input:checkbox').each(function() {
+      checkbox_check(this);
+    });
+
+    // Task check box
+    $('#task-card input:checkbox').change(function() {
+      checkbox_check(this);
+    });
+
+    // Check Uncheck function
+    function checkbox_check(el) {
+      if (!$(el).is(':checked')) {
+        $(el).next().css('text-decoration', 'none'); // or addClass
+      } else {
+        $(el).next().css('text-decoration', 'line-through'); //or addClass
+      }
+    }
+    // Plugin initialization
+
+    $('select').material_select();
+    // Set checkbox on forms.html to indeterminate
+    var indeterminateCheckbox = document.getElementById('indeterminate-checkbox');
+    if (indeterminateCheckbox !== null)
+      indeterminateCheckbox.indeterminate = true;
+
+    // Commom, Translation & Horizontal Dropdown
+    $('.dropdown-button, .translation-button, .dropdown-menu').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrainWidth: false,
+      hover: true,
+      gutter: 0,
+      belowOrigin: true,
+      alignment: 'left',
+      stopPropagation: false
+    });
+    // Notification, Profile & Settings Dropdown
+    $('.notification-button, .profile-button, .dropdown-settings').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrainWidth: false,
+      hover: true,
+      gutter: 0,
+      belowOrigin: true,
+      alignment: 'right',
+      stopPropagation: false
+    });
+
     // Materialize scrollSpy
     $('.scrollspy').scrollSpy();
 
@@ -19,6 +79,26 @@
     $('.tooltipped').tooltip({
       delay: 50
     });
+
+    //Main Left Sidebar Menu
+    $('.sidebar-collapse').sideNav({
+      edge: 'left', // Choose the horizontal origin
+    });
+
+    // Overlay Menu (Full screen menu)
+    $('.menu-sidebar-collapse').sideNav({
+      menuWidth: 240,
+      edge: 'left', // Choose the horizontal origin
+      //closeOnClick:true, // Set if default menu open is true
+      menuOut: false // Set if default menu open is true
+    });
+
+    //Main Left Sidebar Chat
+    $('.chat-collapse').sideNav({
+      menuWidth: 300,
+      edge: 'right',
+    });
+
     // Pikadate datepicker
     $('.datepicker').pickadate({
       selectMonths: true, // Creates a dropdown to control month
