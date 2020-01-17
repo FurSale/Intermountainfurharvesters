6d352d3c81ca7bc550942940305f87c861e33b92
Delbert Johnson <johnsondelbert1@gmail.com>
2020-01-17 13:59:59 -0700

    Update edit_sellers.php

--- pages/sellers/edit_sellers.php
+++ pages/sellers/edit_sellers.php
@@ -237,7 +237,7 @@ require_once("../../includes/begin_html.php");
                     <input tabindex="14" name="items[0][tag_id]" type="text" class="validate tag-ID" tabindex placeholder="Tag ID" style="display:none;">
                   </div>
                   <div class="input-field col s2">
-                    <div style="display: inline;"><label><input name="items[0][unit_of_measure]" value="ct" type="radio" class="radio-count" checked /><span>ct</span></label></div>
+                    <div style="display: inline;"><label><input tabindex="999" tab name="items[0][unit_of_measure]" value="ct" type="radio" class="radio-count" checked /><span>ct</span></label></div>
                     <div style="display: inline;"><label><input name="items[0][unit_of_measure]" Value="lbs" type="radio" class="radio-lbs" /><span>lbs</span></label></div>
                     <div style="display: inline;"><label><input name="items[0][unit_of_measure]" Value="oz" type="radio" class="radio-lbs" /><span>oz</span></label></div>
                   </div>
@@ -264,7 +264,7 @@ require_once("../../includes/begin_html.php");
             </form>
             <div class="row">
               <a href="#btn-add-row" tabindex="0" class="waves-effect waves-light btn" id="btn-add-row"><i class="material-icons left">add_box</i>Add Items</a>
-              <span tabindex="19" class="waves-effect waves-light btn" id="btn-save">Save</span>
+              <span tabindex="0" class="waves-effect waves-light btn" id="btn-save">Save</span>
             </div>
           </div>
           <div id="tab2" class="col s12">
