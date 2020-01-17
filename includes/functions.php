9a631ca3108267a4236991b1dd730cee47f8b0b5
Delbert Johnson <johnsondelbert1@gmail.com>
2020-01-17 09:08:06 -0800

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1,12 +1,15 @@
 <?php
 require_once("globals.php");
 require_once("session.php");
-function require_multi($files) {
+function require_multi($files)
+{
     $files = func_get_args();
-    foreach($files as $file)
+    foreach ($files as $file) {
         require_once($file);
+    }
 }
-function echo_states($selected = null){
+function echo_states($selected = null)
+{
     $us_states = array(
         'AL'=>'Alabama',
         'AK'=>'Alaska',
@@ -64,9 +67,9 @@ function echo_states($selected = null){
     $returnHTML = "<option value=\"\">Select a State</option>";
 
     foreach ($us_states as $key => $value) {
-        if($key == $selected){
+        if ($key == $selected) {
             $returnHTML .= "<option value=\"{$key}\" selected>{$value}</option>";
-        }else{
+        } else {
             $returnHTML .= "<option value=\"{$key}\">{$value}</option>";
         }
     }
@@ -102,6 +105,7 @@ $itemTypes = array(
     'Ermine Skull',
     'Fox Paws',
     'Fur Coat',
+    'Fisher',
     'Fur Headband',
     'Fur Pieces',
     'Goat Skulls',
@@ -141,7 +145,8 @@ $itemTypes = array(
     'Wolf Skull'
 );
 
-function echo_cat(){
+function echo_cat()
+{
     global $itemTypes;
 
     $returnHTML = "";
@@ -152,15 +157,16 @@ function echo_cat(){
     return $returnHTML;
 }
 
-function echo_item_types($selected = null){
+function echo_item_types($selected = null)
+{
     global $itemTypes;
 
     $returnHTML = "<option value=\"\">Select type</option><option value=\"Custom\">Custom</option>";
 
     foreach ($itemTypes as $value) {
-        if($value == $selected){
+        if ($value == $selected) {
             $returnHTML .= "<option value=\"{$value}\" selected>{$value}</option>";
-        }else{
+        } else {
             $returnHTML .= "<option value=\"{$value}\">{$value}</option>";
         }
     }
@@ -168,7 +174,8 @@ function echo_item_types($selected = null){
     return $returnHTML;
 }
 
-function random_generator($length, $usableChars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'){
+function random_generator($length, $usableChars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+{
     $randomString = '';
 
     for ($i = 0; $i < $length; $i++) {
@@ -179,46 +186,49 @@ function random_generator($length, $usableChars = '0123456789abcdefghijklmnopqrs
     return $randomString;
 }
 
-function format_date_timezone($dateTime){
+function format_date_timezone($dateTime)
+{
     $dt = new DateTime($dateTime);
     $dt -> setTimezone(new DateTimeZone($GLOBALS['site_info']['timezone']));
     return $dt->format('m/d/Y, H:i:s');
 }
 
 //Returns array of the highest bid on an item
-function get_highest_bids($itemID){
+function get_highest_bids($itemID)
+{
     global $connection;
 
     $query = "SELECT * FROM `bid` WHERE `seller_item_id` = {$itemID} AND `bid_status` = 'Confirmed' ORDER BY `bid_amount` DESC LIMIT 1";
-    $result=mysqli_query( $connection, $query);
+    $result=mysqli_query($connection, $query);
     confirm_query($result);
-    if(mysqli_num_rows($result) > 0){
+    if (mysqli_num_rows($result) > 0) {
         $bid = mysqli_fetch_array($result);
         $query = "SELECT * FROM `bid` WHERE `seller_item_id` = {$itemID} AND `bid_amount` = {$bid['bid_amount']} AND `bid_status` = 'Confirmed' ORDER BY `date_created` ASC";
-        $result2=mysqli_query( $connection, $query);
+        $result2=mysqli_query($connection, $query);
         confirm_query($result2);
-        return mysqli_fetch_all($result2,MYSQLI_ASSOC);
+        return mysqli_fetch_all($result2, MYSQLI_ASSOC);
     }
     return array();
 }
 
 //Returns winning bid or false if there is none
-function get_winning_bid($itemID){
+function get_winning_bid($itemID)
+{
     global $connection;
 
     //Get the item
     $query = "SELECT * FROM `seller_item` WHERE `id` = {$itemID}";
-    $itemResult=mysqli_query( $connection, $query);
+    $itemResult=mysqli_query($connection, $query);
     confirm_query($itemResult);
     $item = mysqli_fetch_assoc($itemResult);
     //Get first record of the highest bid in case of tie bids
     $query = "SELECT * FROM `bid` WHERE `seller_item_id` = {$item['id']} AND `bid_status` = 'Confirmed' ORDER BY `bid_amount` DESC, `date_created` ASC LIMIT 1";
-    $winResult=mysqli_query( $connection, $query);
+    $winResult=mysqli_query($connection, $query);
     confirm_query($winResult);
-    if(mysqli_num_rows($winResult) > 0){
+    if (mysqli_num_rows($winResult) > 0) {
         $winningBid = mysqli_fetch_assoc($winResult);
         //Make sure the highest bid is above or equal the asking amount
-        if($winningBid['bid_amount'] >= $item['asking']){
+        if ($winningBid['bid_amount'] >= $item['asking']) {
             return $winningBid;
         }
     }
@@ -226,30 +236,32 @@ function get_winning_bid($itemID){
 }
 
 //Returns array of all confirmed bids on an item
-function get_item_bids($itemID){
+function get_item_bids($itemID)
+{
     global $connection;
 
     $bids = array();
     $query = "SELECT * FROM `bid` WHERE `seller_item_id` = {$itemID} AND `bid_status` = 'Confirmed' ORDER BY `bid_amount` DESC, `date_created` DESC";
-    $bidResult=mysqli_query( $connection, $query);
+    $bidResult=mysqli_query($connection, $query);
     confirm_query($bidResult);
-    $bids=mysqli_fetch_all($bidResult,MYSQLI_ASSOC);
+    $bids=mysqli_fetch_all($bidResult, MYSQLI_ASSOC);
 
     return $bids;
 }
 
 //Returns array of all winning bids of a buyer
-function get_buyer_won_bids($buyerID){
+function get_buyer_won_bids($buyerID)
+{
     global $connection;
 
     $winningBids = array();
     $query = "SELECT * FROM `bid` WHERE `buyer_id` = {$buyerID} AND `bid_status` = 'Confirmed'";
-    $bidResult=mysqli_query( $connection, $query);
+    $bidResult=mysqli_query($connection, $query);
     confirm_query($bidResult);
     //Check each of the buyer's bid to see if it's the winning one for that item
-    while($bid=mysqli_fetch_assoc($bidResult)){
+    while ($bid=mysqli_fetch_assoc($bidResult)) {
         $winningBid = get_winning_bid($bid['seller_item_id']);
-        if($winningBid !== false && $winningBid['buyer_id'] == $buyerID){
+        if ($winningBid !== false && $winningBid['buyer_id'] == $buyerID) {
             //We should be down here if this is the winning bid
             array_push($winningBids, $winningBid);
         }
@@ -258,19 +270,19 @@ function get_buyer_won_bids($buyerID){
 }
 
 //Returns array of sold items under a seller
-function get_seller_sold_items($sellerID){
+function get_seller_sold_items($sellerID)
+{
     global $connection;
 
     $soldItems = array();
     $query = "SELECT * FROM `seller_item` WHERE `seller_id` = {$sellerID}";
-    $sellerItemResult=mysqli_query( $connection, $query);
+    $sellerItemResult=mysqli_query($connection, $query);
     confirm_query($sellerItemResult);
     //Check each of the seller's items to see if it's sold
-    while($item=mysqli_fetch_assoc($sellerItemResult)){
-        if(get_winning_bid($item['id']) !== false){
+    while ($item=mysqli_fetch_assoc($sellerItemResult)) {
+        if (get_winning_bid($item['id']) !== false) {
             array_push($soldItems, $item);
         }
     }
     return $soldItems;
 }
-?>
