59ac2a17a7b7cb331121eb13e281ac57793fff4e
Delbert Johnson <johnsondelbert1@gmail.com>
2020-01-18 15:44:29 -0700

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -1,95 +1,87 @@
 <?php
 require_once("../includes/db_connection.php");
 $pgsettings = array(
-	"title" => "Login to Intermountain Fur Harvesters",
-	"icon" => "icon-newspaper"
+    "title" => "Login to Intermountain Fur Harvesters",
+    "icon" => "icon-newspaper"
 );
 require_once("../includes/functions.php");
 
-if(logged_in()){
-	header("Location: sellers/list_sellers.php");
+if (logged_in()) {
+    header("Location: sellers/list_sellers.php");
 }
 
 $login['username'] = "";
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
+    function setLoginCookie($found_user)
+    {
+        global $connection;
+        $_SESSION['user_id']=$found_user['id'];
+        $_SESSION['username']=$found_user['username'];
+        // if(isset($_POST['remember'])){$remember=1;}else{$remember=0;}
+        // if($remember==1){
+        // 	setcookie("rememberme",$found_user['username'], time()+60*60*24*30, '/');
+        // }
 
-	function setLoginCookie($found_user){
-		global $connection;
-		$_SESSION['user_id']=$found_user['id'];
-		$_SESSION['username']=$found_user['username'];
-		// if(isset($_POST['remember'])){$remember=1;}else{$remember=0;}
-		// if($remember==1){
-		// 	setcookie("rememberme",$found_user['username'], time()+60*60*24*30, '/');
-		// }
+        //Login cookie is set for 12 hours
+        setcookie("rememberme", $found_user['username'], time()+60*60*12, '/');
 
-		//Login cookie is set for 12 hours
-		setcookie("rememberme",$found_user['username'], time()+60*60*12, '/');
-		
-		session_name("login");
-		session_start();
-		
-		$date=date("Y-m-d H:i:s");
-		$query = "";
-		//Set a new one time pass if user is a buyer
-		if($found_user['role'] == "buyer"){
-			$randomPass = random_generator(6, "0123456789");
-			$query="UPDATE `user` SET `password_one_time` = '{$randomPass}', `date_last_logged_in` = '{$date}' 
-			WHERE `id` = {$_SESSION['user_id']}";
-		}else{
-			$query="UPDATE `user` SET `date_last_logged_in` = '{$date}' 
-			WHERE `id` = {$_SESSION['user_id']}";
-		}
+        session_name("login");
+        session_start();
 
-		$result=mysqli_query($connection, $query);
-		confirm_query($result);
+        $date=date("Y-m-d H:i:s");
+        $query = "";
+        //Set a new one time pass if user is a buyer
+        
 
-		if($found_user['role'] == "buyer"){
-			header("Location: frontend/index.php");
-		}else{
-			header("Location: sellers/list_sellers.php");
-		}
-		
-	}
+        $result=mysqli_query($connection, $query);
+        confirm_query($result);
 
-	if(!isset($_POST['username']) || !isset($_POST['password'])){
-		header("Location: login.php");
-	}
+        if ($found_user['role'] == "buyer") {
+            header("Location: frontend/index.php");
+        } else {
+            header("Location: sellers/list_sellers.php");
+        }
+    }
 
-	$user=trim($_POST['username']);
-	$pass=$_POST['password'];
+    if (!isset($_POST['username']) || !isset($_POST['password'])) {
+        header("Location: login.php");
+    }
 
-	if($user == '' || $pass == ''){
-		$error = "field left blank";
-	}
-	
-	$query="SELECT * FROM user
+    $user=trim($_POST['username']);
+    $pass=$_POST['password'];
+
+    if ($user == '' || $pass == '') {
+        $error = "field left blank";
+    }
+
+    $query="SELECT * FROM user
 			WHERE `username` = '{$user}'";
-	$result=mysqli_query($connection, $query);
-	confirm_query($result);
-	if (mysqli_num_rows($result)==1){
-		$found_user = mysqli_fetch_array($result);
-		if($found_user['role'] == "buyer"){
-			if($pass == $found_user['password_one_time']){
-				setLoginCookie($found_user);
-			}else{
-				$error = "Wrong password";
-			}
-		}else{
-			//Check if hashed password matches DB password
-			if(password_verify($pass, $found_user['password'])){
-				setLoginCookie($found_user);
-			}else{
-				$error = "Wrong password";
-			}
-		}
-		$error = "User does not exist";
-	}
-}else{
-	if(isset($_GET['logout']) && $_GET['logout']==1){
-		$success = "You have successfully logged out!";
-	}
-	//redirect_to("../login.php");
+    $result=mysqli_query($connection, $query);
+    confirm_query($result);
+    if (mysqli_num_rows($result)==1) {
+        $found_user = mysqli_fetch_array($result);
+        if ($found_user['role'] == "buyer") {
+            if ($pass == $found_user['password_one_time']) {
+                setLoginCookie($found_user);
+            } else {
+                $error = "Wrong password";
+            }
+        } else {
+            //Check if hashed password matches DB password
+            if (password_verify($pass, $found_user['password'])) {
+                setLoginCookie($found_user);
+            } else {
+                $error = "Wrong password";
+            }
+        }
+        $error = "User does not exist";
+    }
+} else {
+    if (isset($_GET['logout']) && $_GET['logout']==1) {
+        $success = "You have successfully logged out!";
+    }
+    //redirect_to("../login.php");
 }
 require_once("../includes/begin_html.php");
 ?>
